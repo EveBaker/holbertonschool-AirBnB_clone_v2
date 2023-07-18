0d88214dd4ccead92021ab471a6d4068ac82f931
EveBaker <EveBaker0162@gmail.com>
2023-07-18 22:58:34 +0000

    flask

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application."""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """Remove the current SQLAlchemy Session."""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays a HTML page with the list of all state objexts and thir link"""
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+
+    return render_template('8-cities_byby_states.html', states=sorted_states)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
